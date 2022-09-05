f26b02ce6b0444cd611ef619efdc8c0034f99adb
ArianeCMachado <109089136+ArianeCMachado@users.noreply.github.com>
2022-09-05 16:32:16 -0300

    Cards prontos

--- src/main.js
+++ src/main.js
@@ -1,5 +1,31 @@
-import { example } from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
 
+const divCharacter=document.querySelector(".characters")
 
-console.log(example, data);
+const characters = data.results
+const PrintarCards =lista => {
+  const cards =lista.map(function(character){
+    const cardHtml = ` <div class="cards"> <li>
+    <img src = "${character.image}" class = "card-poster">
+    <ul class ="cardText">
+    <b>${character.name}</b>
+    <br>
+    Status: ${character.status}
+    <br>
+    Gender: ${character.gender}
+    <br>
+    Species: ${character.species}
+    </ul> </li> </div>`
+    return cardHtml
+  })
+  divCharacter.innerHTML=cards
+}
+PrintarCards(characters)
+
+/*const orderAZ = (characters) => {
+  return orderAZ(character.name).sort()
+};
+
+const orderZA = (item) => {
+  return orderZA(item).reverse();
+};*/
